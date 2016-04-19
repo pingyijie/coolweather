414fd2ab47cf46c022b1c23cf2e42ec853f68e47
tony <tony@gmail.com>
2016-04-19 17:38:35 +0800

    新增数据库帮助类，以及各表对应的实体类.

---
+++ app/src/main/java/model/Province.java
@@ -0,0 +1,34 @@
+package model;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
--- app/src/main/java/ping/qf/com/coolweather1/activity/MainActivity.java
+++ app/src/main/java/ping/qf/com/coolweather1/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package ping.qf.com.coolweather1;
+package ping.qf.com.coolweather1.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ping.qf.com.coolweather1.R;
 
 public class MainActivity extends AppCompatActivity {
 
